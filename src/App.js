32c662fbfc0aff57386aaddac0353cf141d2dca4
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-04-05 08:22:28 -0400

    Finishes Hackathon project by making css injectable from chrome extension

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* global chrome */
 import React, { useState } from 'react';
 
 import AceEditor from 'react-ace';
@@ -5,21 +6,20 @@ import 'ace-builds/src-noconflict/mode-css';
 import 'ace-builds/src-noconflict/theme-chrome';
 
 function App() {
-  const [currentStyle, setCurrentStyle] = useState(``)
+  const [currentStyle, setCurrentStyle] = useState(``);
+  const chromeTabsCSS = (input) => {
+    chrome.tabs.insertCSS(null, {code: input})
+    setCurrentStyle(input)
+  }
   return (
     <div style={{minHeight: "200px", minWidth: "400px"}}>
       <AceEditor
       mode="css"
       theme='chrome'
-      onChange={setCurrentStyle}
+      onChange={chromeTabsCSS}
       value={currentStyle}
       name="CSS-EDITOR"
       />
-      <style dangerouslySetInnerHTML={{
-        __html: currentStyle
-      }}>
-
-      </style>
     </div>
   );
 }
